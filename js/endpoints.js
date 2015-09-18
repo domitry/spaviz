948ec788a2d31866ebee4c3d9648a45e4bd9aa39
Naoki Nishida <domitry@gmail.com>
2015-09-19 00:14:00 +0900

    Enable uniprot and change the order of endpoints

--- js/endpoints.js
+++ js/endpoints.js
@@ -1,6 +1,14 @@
 /*global require, module, _, d3*/
 
 module.exports = {
+    "nibb": {
+        mode: "bif",
+        uri: "http://sparql.nibb.ac.jp/sparql"
+    },
+    "uniprot": {
+        mode: "bif",
+        uri: "http://sparql.uniprot.org/sparql"
+    },
     dbpedia: {
         mode: "ordinal",
         uri: "http://dbpedia.org/sparql"
@@ -8,9 +16,5 @@ module.exports = {
     "dbpedia-jp": {
         mode: "bif",
         uri: "http://ja.dbpedia.org/sparql"
-    },
-    "nibb": {
-        mode: "bif",
-        uri: "http://sparql.nibb.ac.jp/sparql"
     }
 };
